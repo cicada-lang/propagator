fa3eec7ac84b9e6221d5ee1f633f26987c2a24f3
Xie Yuheng <xyheme@gmail.com>
2024-06-18 08:40:23 +0800

    add `generics/quadratic.test.ts`

---
+++ src/generics/quadratic.test.ts
@@ -0,0 +1,8 @@
+import assert from "node:assert"
+import test from "node:test"
+import { sqrt, square } from "./quadratic.js"
+
+test("quadratic", async () => {
+  assert.deepStrictEqual(square(2), 4)
+  assert.deepStrictEqual(sqrt(4), 2)
+})
