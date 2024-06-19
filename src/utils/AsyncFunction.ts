9da28837c51d8cfdf6bdb5df30c065aa5af0b091
Xie Yuheng <xyheme@gmail.com>
2024-06-19 21:45:12 +0800

    up

--- src/utils/AsyncFunction.ts
+++ src/utils/AsyncFunction.ts
@@ -1,7 +1,3 @@
 const anAsyncFunction = async () => {}
 
 export const AsyncFunction = anAsyncFunction.constructor
-
-export function isAsyncFunction(fn: Function): fn is typeof anAsyncFunction {
-  return fn instanceof AsyncFunction
-}
