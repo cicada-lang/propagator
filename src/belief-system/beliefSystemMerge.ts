24af62fad170ee9a9dc44ee35904996e06d9a129
Xie Yuheng <xyheme@gmail.com>
2024-09-02 19:46:19 +0800

    up

--- src/belief-system/beliefSystemMerge.ts
+++ src/belief-system/beliefSystemMerge.ts
@@ -1,43 +1,54 @@
-import { Belief, isBelief } from "../belief/index.js"
+import { Belief } from "../belief/index.js"
 import { isNothing, nothing, type Nothing } from "../cell/index.js"
 import { implies, merge, type MergeConflict } from "../merge/index.js"
 import type { Reasons } from "../reason/index.js"
 import { setIsSubsetOf } from "../utils/set/index.js"
 import { BeliefSystem } from "./BeliefSystem.js"
 
+// Asking the TMS to deduce all the consequences of all its facts all
+// the time is perhaps a bad idea, so when we merge TMSes we
+// assimilate the facts from the incoming one into the current one,
+// and then deduce only those consequences that are relevant to the
+// current worldview.
+
 export function beliefSystemMerge<A, B>(
   content: BeliefSystem<A>,
   increment: BeliefSystem<B>,
 ): BeliefSystem<A | B> | MergeConflict {
-  const candidate = beliefSystemAssimilate(content, increment)
+  const candidate = assimilate(content, increment)
   const consequence = strongestConsequence(candidate)
-  return beliefSystemAssimilate(candidate, consequence)
+  return assimilateOne(candidate, consequence)
 }
 
-function beliefSystemAssimilate<A, B>(
+// The procedure `assimilate` incorporates all the given items, one by
+// one, into the given TMS with no deduction of consequences.
+
+function assimilate<A, B>(
   base: BeliefSystem<A>,
-  target: BeliefSystem<B> | Belief<B> | Nothing,
+  target: BeliefSystem<B> | Nothing,
 ): BeliefSystem<A | B> {
   if (isNothing(target)) {
     return base
-  } else if (isBelief(target)) {
-    return beliefSystemAssimilateOne(base, target)
-  } else {
-    return target.beliefs.reduce<BeliefSystem<A | B>>(
-      (result, belief) => beliefSystemAssimilateOne(result, belief),
-      base,
-    )
   }
+
+  return target.beliefs.reduce<BeliefSystem<A | B>>(
+    (result, belief) => assimilateOne(result, belief),
+    base,
+  )
 }
 
 function subsumes<A, B>(x: Belief<A>, y: Belief<B>): boolean {
   return implies(x.value, y.value) && setIsSubsetOf(x.reasons, y.reasons)
 }
 
-function beliefSystemAssimilateOne<A, B>(
+function assimilateOne<A, B>(
   base: BeliefSystem<A>,
-  belief: Belief<B>,
+  belief: Belief<B> | Nothing,
 ): BeliefSystem<A | B> {
+  if (isNothing(belief)) {
+    return base
+  }
+
   if (base.beliefs.some((oldBelief) => subsumes(oldBelief, belief))) {
     return base
   }
