99eb5ca065c6351b91d2135134bb83cffdb1a099
Xie Yuheng <xyheme@gmail.com>
2024-09-04 20:14:47 +0800

    extract `isStrongerBelief`

--- src/belief-system/beliefSystemMerge.ts
+++ src/belief-system/beliefSystemMerge.ts
@@ -1,8 +1,8 @@
 import { Belief } from "../belief/index.js"
-import { implies, type MergeConflict } from "../merge/index.js"
+import { type MergeConflict } from "../merge/index.js"
 import { isNothing, type Nothing } from "../nothing/index.js"
-import { setIsSubsetOf } from "../utils/set/index.js"
 import { BeliefSystem } from "./BeliefSystem.js"
+import { isStrongerBelief } from "./isStrongerBelief.js"
 import { strongestBelief } from "./strongestBelief.js"
 
 // Asking the belief system to deduce all the consequences of all its
@@ -51,7 +51,7 @@ function assimilateOne<A, B>(
   // from that in some beliefs already in the belief system. If so, we
   // can just throw the new one away.
 
-  if (base.beliefs.some((oldBelief) => isStronger(oldBelief, belief))) {
+  if (base.beliefs.some((oldBelief) => isStrongerBelief(oldBelief, belief))) {
     return base
   }
 
@@ -64,31 +64,8 @@ function assimilateOne<A, B>(
   // 前者还包含了偏序关系中不可比较的情况。
 
   const strongerOldBeliefs = base.beliefs.filter(
-    (oldBelief) => !isStronger(belief, oldBelief),
+    (oldBelief) => !isStrongerBelief(belief, oldBelief),
   )
 
   return BeliefSystem<A | B>([...strongerOldBeliefs, belief])
-}
-
-// The predicate `isStronger` returns true only if the information
-// contained in the second argument is deducible from that contained
-// in the first.
-
-// About stronger belief:
-// - A belief has stronger value is stronger.
-// - A belief has less reasons is stronger.
-
-// 注意，下面的 isStronger 与用 merge 所定义的 implies 不同，
-// merge 两个 belief 时，用的是 reasons 集合之间的并，
-// 因此 implies(x, y) 对应的是 x.reasons 是 y.reasons 的超集。
-
-// 按照 `beliefMerge` 的定义，可以获得 `Belief` 的，
-// 用 `merge` 定义的 ordered set，其序关系为 `implies`，
-// 这与 `isStronger` 这个需关系所定义的 `Belief` 的 ordered set 是不同的。
-
-// 也就是说，`Belief` 这种复合 ordered set 有多种定义方式，
-// 在定义 `beliefSystemMerge` 时，用到的不是 `merge` 所衍生出来的那个。
-
-function isStronger<A, B>(x: Belief<A>, y: Belief<B>): boolean {
-  return implies(x.value, y.value) && setIsSubsetOf(x.reasons, y.reasons)
 }
---
+++ src/belief-system/isStrongerBelief.ts
@@ -0,0 +1,24 @@
+import type { Belief } from "../belief/Belief.js"
+import { implies } from "../merge/merge.js"
+import { setIsSubsetOf } from "../utils/set/Set.js"
+
+// The predicate `isStrongerBelief` returns true only if the
+// information contained in the second argument is deducible from that
+// contained in the first.
+
+// About stronger belief:
+// - A belief has stronger value is stronger.
+// - A belief has less reasons is stronger.
+
+// 注意，下面的 isStrongerBelief 与用 merge 所定义的 implies 不同，
+// merge 两个 belief 时，用的是 reasons 集合之间的并，
+// 因此 implies(x, y) 对应的是 x.reasons 是 y.reasons 的超集。
+// 按照 `beliefMerge` 的定义，可以获得 `Belief` 的，
+// 用 `merge` 定义的 ordered set，其序关系为 `implies`，
+// 这与 `isStrongerBelief` 这个需关系所定义的 `Belief` 的 ordered set 是不同的。
+// 也就是说，`Belief` 这种复合 ordered set 有多种定义方式，
+// 在定义 `beliefSystemMerge` 时，用到的不是 `merge` 所衍生出来的那个。
+
+export function isStrongerBelief<A, B>(x: Belief<A>, y: Belief<B>): boolean {
+  return implies(x.value, y.value) && setIsSubsetOf(x.reasons, y.reasons)
+}
