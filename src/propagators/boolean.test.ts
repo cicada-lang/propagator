fb2d2f0e2cdfb5a7a1574b8db137bb280dcab1a0
Xie Yuheng <xyheme@gmail.com>
2024-09-30 09:07:53 +0800

    propagators/boolean -- conjoiner & disjoiner

--- src/propagators/boolean.test.ts
+++ src/propagators/boolean.test.ts
@@ -2,13 +2,29 @@ import assert from "node:assert"
 import test from "node:test"
 import { patch } from "../cell/index.js"
 import { run } from "../scheduler/index.js"
-import { inverter } from "./boolean.js"
+import { conjoiner, disjoiner, inverter } from "./boolean.js"
 
 test("propagators / boolean", async () => {
-  const [x, y] = inverter()
-  patch(x, true)
+  {
+    const [x, y] = inverter()
+    patch(x, true)
+    await run()
+    assert(y.content === false)
+  }
 
-  await run()
+  {
+    const [x, y, z] = conjoiner()
+    patch(x, true)
+    patch(y, false)
+    await run()
+    assert(z.content === false)
+  }
 
-  assert(y.content === false)
+  {
+    const [x, y, z] = disjoiner()
+    patch(x, true)
+    patch(y, false)
+    await run()
+    assert(z.content === true)
+  }
 })
--- src/propagators/boolean.ts
+++ src/propagators/boolean.ts
@@ -1,4 +1,6 @@
-import { not } from "../generics/index.js"
+import { and, not, or } from "../generics/index.js"
 import { definePrimitive } from "../propagator/index.js"
 
 export const inverter = definePrimitive(2, not)
+export const conjoiner = definePrimitive(3, and)
+export const disjoiner = definePrimitive(3, or)
