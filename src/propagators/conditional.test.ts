d959d36649927e548e99b14e429a23081c3006cb
Xie Yuheng <xyheme@gmail.com>
2024-09-30 09:28:00 +0800

    up

--- src/propagators/conditional.test.ts
+++ src/propagators/conditional.test.ts
@@ -1,8 +1,9 @@
 import assert from "node:assert"
 import test from "node:test"
 import { patch } from "../cell/index.js"
+import { nothing } from "../nothing/index.js"
 import { run } from "../scheduler/index.js"
-import { conditional } from "./conditional.js"
+import { conditional, conditionalWriter } from "./conditional.js"
 
 test("propagators / conditional", async () => {
   {
@@ -12,5 +13,14 @@ test("propagators / conditional", async () => {
     patch(ifTrue, 1)
     await run()
     assert(output.content === 1)
+  }
+
+  {
+    const [control, input, ifTrue, ifFalse] = conditionalWriter()
+    patch(control, true)
+    patch(input, 6)
+    await run()
+    assert(ifTrue.content === 6)
+    assert(ifFalse.content === nothing)
   }
 })
