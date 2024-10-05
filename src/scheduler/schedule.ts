1a22a1db2428edaf923ee109f59bbd4ee1f5c48e
Xie Yuheng <xyheme@gmail.com>
2024-10-05 21:48:28 +0800

    `processNogood` -- remove abort, i do not understand yet, maybe need call/cc

--- src/scheduler/schedule.ts
+++ src/scheduler/schedule.ts
@@ -1,7 +1,7 @@
 import type { Propagator } from "../propagator/index.js"
 import { globalScheduler } from "./initializeScheduler.js"
 
-let allEverScheduledPropagators = new Set<Propagator>()
+const allEverScheduledPropagators = new Set<Propagator>()
 
 export function schedule(propagators: Array<Propagator>): void {
   for (const propagator of propagators) {
@@ -15,9 +15,4 @@ export function schedule(propagators: Array<Propagator>): void {
 
 export function scheduleAllEverScheduledPropagators(): void {
   schedule(Array.from(allEverScheduledPropagators))
-}
-
-export function globalSchedulerAbort(): void {
-  globalScheduler.jobs = []
-  // allEverScheduledPropagators = new Set()
 }
